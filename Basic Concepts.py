4b34f6a80d1321a2e8e207a46e12c320b9ca5f9c
diyagarg-23 <diyashalu.2302@gmail.com>
2024-09-30 09:21:04 +0530

    Add files via upload

---
+++ Basic Concepts.py
@@ -0,0 +1,96 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+#writing first code
+print('Hello everyone! My name is Diya Garg')
+
+
+# In[7]:
+
+
+#data types
+#integer , string , Boolean, Float, None
+a=3
+b=5
+print(a+b)
+
+print(type(a))
+print(type(6.7))
+print((3>6))
+type(3>6)
+
+c='Diya Garg'
+print(type(c))
+
+d=None #for empty
+
+
+# In[10]:
+
+
+#variables
+#case-sensitive
+a=6 #here a is variable
+
+
+# In[12]:
+
+
+#allow user to enter
+a= input("enter a: ")
+b= input("enter b: ")
+print(a+b) 
+#output is coming out wrong because by default input function takes arguement as string
+
+
+# In[13]:
+
+
+#converting to int known as type casting
+a= int(input("enter a: "))
+b= int(input("enter b: "))
+print(a+b)
+
+
+# In[19]:
+
+
+#arithmetic operators
+print(9-2) #subtraction
+print(6/2)
+print(6//2) #prints only whole number
+print(5%2) #remainder
+print(5*7) #multuplication
+
+
+# In[23]:
+
+
+#logical operators
+print((3>0) and (10>11))
+print((3>0) or (10<11))
+print(not((3>0) or (10<11))) #negate the result
+
+
+# In[25]:
+
+
+#string concatenation
+'Diya' + ' Garg'
+
+
+# In[27]:
+
+
+#repitition
+'Diya ' * 4
+
+
+# In[ ]:
+
+
+
+
